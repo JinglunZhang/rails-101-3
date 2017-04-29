153b440159c2a977f2ed700bdee8e562b81c4013
jinglunzhang <jinglunzhang2017@gmail.com>
2017-04-29 08:51:31 -0400

    use simple_format helper to decorate

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -4,4 +4,16 @@ class Account::GroupsController < ApplicationController
   def index
     @groups = current_user.participated_groups
   end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    @group.update(group_params)
+
+    redirect_to groups_path, notice: "Update Success"
+  end
 end
